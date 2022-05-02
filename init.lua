f477c31e7533f6d14d9b31ff294468e150c321a4
rudzik8 <rudzik8@protonmail.com>
2022-05-02 14:52:50 +0700

    OOOOOOPS

--- init.lua
+++ init.lua
@@ -37,8 +37,8 @@ minetest.register_chatcommand("sit", {
 			mcl_player.player_attached[name] = true
 			mcl_player.player_set_animation(player, "sit", 30)
 			if mcl_cozy_print_actions then
-				minetest.chat_send_all("* "..name..S(" sits")
-			end,
+				minetest.chat_send_all("* "..name..S(" sits"))
+			end
 			mcl_title.set(player, "actionbar", {text=S("Move to stand up"), color="white", stay=60})
 		end
 	end
@@ -63,7 +63,7 @@ minetest.register_chatcommand("lay", {
 			mcl_player.player_set_animation(player, "lay", 0)
 			if mcl_cozy_print_actions then
 				minetest.chat_send_all("* "..name..S(" lies"))
-			end,
+			end
 			mcl_title.set(player, "actionbar", {text=S("Move to stand up"), color="white", stay=60})
 		end
 	end
